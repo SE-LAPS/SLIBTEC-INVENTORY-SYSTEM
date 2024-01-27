66e76ec7a5da18a43effe5af53a6c21c71f84ddc
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-01-28 01:03:02 +0530

    Update Verify.php

--- OTP/Verify.php
+++ OTP/Verify.php
@@ -1,21 +1,13 @@
 <?php
 
-namespace SanjabVerify\Support\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-/**
- * @see \SanjabVerify\Verify
- */
-class Verify extends Facade
-{
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor()
-    {
-        return 'verify';
-    }
-}
+return [
+    'sent_successfully' => 'Verification code was sent successfully.',
+    'send_failed' => 'Sending verification code failed. Please try later.',
+    'resend_wait' => 'You have requested for verification code recently. please retry in :seconds seconds.',
+    'too_many_requests' => 'You requested too many verification codes. please retry in next hour.',
+    'code_expired' => 'Your code has expired. Please request another verification code.',
+    'verified_successfully' => 'Your code verified successfully.',
+    'code_is_not_yours' => 'This code is not your\'s. Please request another verification code.',
+    'code_is_wrong' => 'Code is wrong.',
+    'code_attempt_limited' => 'You attempted to verify code more than :count times. Please request another verification code.',
+];
